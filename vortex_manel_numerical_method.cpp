030248e88837c78907f4c001bae70c7f9602afe6
scienceasdf <scienceasdf@126.com>
2016-09-10 22:56:36 +0800

    Update vortex_manel_numerical_method.cpp

--- vortex_manel_numerical_method.cpp
+++ vortex_manel_numerical_method.cpp
@@ -22,10 +22,10 @@ int main()
     double alpha=.0*tPi/360.0;
     double airDen=1.225;
 
-    double px[100],py[100],ctrlpx[100],ctrlpy[100],nx[100],ny[100],s[100];
+    double px[100],py[100],ctrlpx[100],ctrlpy[100],nx[100],ny[100],s[100],phi[100];
     double beta[100];
 
-
+    double aa,bb,cc,dd,ee;
 
     //At present here I use array instead of std::vector
     Eigen::MatrixXd A;
@@ -59,12 +59,13 @@ int main()
 
     double dx=.0,dy=.0;
     for(int i=0;i<n-1;++i){
+        phi[i]=std::atan2(py[i+1]-py[i],px[i+1]-px[i]);
         ctrlpx[i]=(px[i]+px[i+1])/2.0;
         ctrlpy[i]=(py[i]+py[i+1])/2.0;
         nx[i]=py[i]-py[i+1];
         ny[i]=px[i+1]-px[i];        //nx, ny represents the x and y components of the normal vector
-        beta[i]=std::atan2(nx[i],.0-ny[i])+alpha;
-        v[i]=vInf*beta[i];
+        beta[i]=std::atan2(.0-ny[i],nx[i])+alpha;
+        v[i]=vInf*cos(beta[i]);
         s[i]=pow((py[i]-py[i+1])*(py[i]-py[i+1])+(px[i+1]-px[i])*(px[i+1]-px[i]),.5);
 
     }
@@ -74,11 +75,21 @@ int main()
 
     for(int i=0;i<n;++i){
         for(int j=0;j<n;++j){
+            aa=-(px[i]-px[j])*cos(phi[j])-(py[i]-py[j])*sin(phi[j]);
+            bb=(px[i]-px[j])*(px[i]-px[j])+(py[i]-py[j])*(py[i]-py[j]);
+            cc=sin(phi[i]-phi[j]);
+            dd=(py[i]-py[j])*cos(phi[i])-(px[i]-px[j])*sin(phi[i]);
+            ee=pow(bb-aa*aa,.5);
+
             dx=ctrlpx[i]-ctrlpx[j];
             dy=ctrlpy[i]-ctrlpy[j];
-            cj=1.0/(1.0+dy*dy/dx/dx);
-            cj=cj-(dy/dx/dx*nx[i]+1.0/dx*ny[i]);
-            A(i,j)=(i==j)?0:cj*s[j]/tPi;            //Maybe here A(i,j) use the reload of the operator ()
+            /*cj=1.0/(1.0+dy*dy/dx/dx);
+            cj=cj-(dy/dx/dx*nx[i]+1.0/dx*ny[i]);*/
+
+            cj=.5*cc*std::log((s[j]*s[j]+2.0*aa*s[j]+bb)/bb)+
+                    (dd-aa*cc)/ee*(std::atan2(s[j]+aa,ee)-std::atan2(aa,ee));
+            std::cout<<cj<<"\n";
+            A(i,j)=(i==j)?.5:cj/tPi;            //Maybe here A(i,j) use the reload of the operator ()
         }
     }
     for(int i=1;i<n;++i){
